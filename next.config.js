8ad9069ce3d4b35cd112e6fbbaa5b37ce18ad8a2
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2022-01-02 17:52:39 +0000

    connect to mongodb

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    MONGO_URI: process.env.MONGO_URI,
+    DEV_URL: process.env.DEV_URL,
+    PROD_URL: process.env.PROD_URL,
+  },
+};
---
+++ utils/mongo.js
@@ -0,0 +1,45 @@
+import { MongoClient } from "mongodb";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+// Check if the mongodb uri exists
+if (!MONGO_URI) {
+  throw new Error("Define the MONGO_URI environment variable.");
+}
+
+let cachedClient = null;
+let cachedDb = null;
+
+export async function connectToDatabase() {
+  // Check if cached
+  if (cachedClient && cachedDb) {
+    return {
+      client: cachedClient,
+      db: cachedDb,
+    };
+  }
+
+  // Set options for configuration
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  // Connect to the cluster
+
+  let client = new MongoClient(MONGO_URI, options);
+
+  await client.connect();
+
+  let db = client.db("Typer");
+
+  // Set cache
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return {
+    client: cachedClient,
+    db: cachedDb,
+  };
+}
